d9870de369da8cf13210cd9716e5e0d84848ae3e
mutakeenbhat <90750027+mutakeenbhat@users.noreply.github.com>
2024-09-23 00:04:10 +0530

    Add files via upload

--- ATMSIMULATOR.py
+++ ATMSIMULATOR.py
@@ -1,20 +1,29 @@
+initial_balance=1000
+def withdraw(x):
+     global initial_balance
+     initial_balance-=x
+     return initial_balance
+def deposit(y):
+     global initial_balance
+     initial_balance+=y
+     return initial_balance
 while True:
- initial_balance=1000
- def withdraw(x):
-    return initial_balance-x
- def deposit(y):
-    return y+initial_balance
  ask=input("do you want to check (balance/deposit/withdraw) or type 'exit' ? ")
  if ask=='exit':
-    break
+  break
  if ask=='balance':
-    print("the current balance is",initial_balance)
+    print(f"the current balance is",{initial_balance})
  elif ask=='deposit':
-    ask2=int(input(print("enter the amount to be deposited $")))
-    print(f"the new balance is ${deposit(ask2)}")
+    ask2=int(input("enter the amount to be deposited $ "))
+    new_bal=deposit(ask2)
+    print(f"the new balance is ${new_bal}")
  elif ask=='withdraw':
-    ask3=int(input(print("enter the amount to be deposited $")))
-    print(f"the remaining balance after withdrawing is ${withdraw(ask3)}")
+    ask3=int(input("enter the amount to be deposited $"))
+    if ask3>initial_balance:
+       print("insufficient balance")
+    else:
+     new_bal=withdraw(ask3)
+     print(f"the remaining balance after withdrawing is ${new_bal}")
  else:
     print("not a valid option")
     
